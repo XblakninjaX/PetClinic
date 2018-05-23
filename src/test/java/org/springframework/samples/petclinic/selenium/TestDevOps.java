559d27e6173c8cb75d9df348f11797390ce3ff76
Administrator <admin@example.com>
2018-05-23 14:08:22 +0000

    Update TestDevOps.java

--- src/test/java/org/springframework/samples/petclinic/selenium/TestDevOps.java
+++ src/test/java/org/springframework/samples/petclinic/selenium/TestDevOps.java
@@ -7,14 +7,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-/*import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;*/
-/*import org.junit.AfterClass;
+import org.testng.annotations.Test;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -24,7 +24,7 @@ public class TestDevOps {
 
 	static String Url = "http://ec2-52-222-114-175.us-gov-west-1.compute.amazonaws.com:9005/spring-petclinic-4.2.5-SNAPSHOT/";
 
-/*	@BeforeClass
+	@BeforeClass
 
 	public void SetUp() throws IOException
 
@@ -34,17 +34,17 @@ public class TestDevOps {
 		driver.get(Url);
 		driver.manage().window().maximize();
 		WebDriverWait wait = new WebDriverWait(driver, 30);
-	}*/
+	}
 	
-	/*@BeforeClass
+	@BeforeClass
 	public static void startDriver() {
 		//driver = Utils.getDriver();
 		//System.setProperty("webdriver.gecko.driver","C:\\Users\\mahmun.rahman\\Desktop\\TSPi\\DevOps\\Development\\Library\\geckodriver.exe");
 		//driver = new FirefoxDriver();
-	/*	System.setProperty("webdriver.ie.driver","C:\\Users\\mahmun.rahman\\Desktop\\TSPi\\Development\\Library\\IEDriverServer_x64_3.12.0\\IEDriverServer.exe"); 
-		driver =new InternetExplorerDriver(); */
+		System.setProperty("webdriver.ie.driver","C:\\Users\\mahmun.rahman\\Desktop\\TSPi\\Development\\Library\\IEDriverServer_x64_3.12.0\\IEDriverServer.exe"); 
+		driver =new InternetExplorerDriver(); 
 		
-	/*	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get(Url);
 		driver.manage().window().maximize();
 		WebDriverWait wait = new WebDriverWait(driver, 30);
@@ -54,7 +54,7 @@ public class TestDevOps {
 	
 	
 
-	//@Test(priority = 1)
+	@Test(priority = 1)
 	@Test
 	public void addOwner() throws IOException, InterruptedException {
 
@@ -134,17 +134,18 @@ public class TestDevOps {
 
 	}
 
-/*	@AfterClass
+	@AfterClass
 
 	public void teardown() {
 
 		driver.close();
 	}
-	*/
-/*	@AfterClass
+	
+	@AfterClass
 	public static void stopDriver() {
 		driver.quit();
 	}
 
 }
+*/
 
